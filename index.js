fd3f3c9cba641c00ab5f86ab4023812228f3ed91
Joseph Plair <jpleeze@yahoo.com>
2023-05-09 20:38:23 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(superbowlData) {
+    const win = superbowlData.find(superbowl => superbowl.result === "W");
+    return win ? win.year : undefined;
+}
